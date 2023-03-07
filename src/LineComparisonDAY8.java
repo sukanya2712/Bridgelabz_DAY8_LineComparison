bfc5749a15d9c21a9f87dc581ce3d86172f60ece
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-07 19:54:53 +0530

    ADDED_UC1Lengthofline

--- src/LineComparisonDAY8.java
+++ src/LineComparisonDAY8.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class LineComparisonDAY8 {
     public static void main(String[] args) {
+        double x1,x2,y1,y2;
 
-       /* Start with Displaying Welcome to Line Comparison Computation  Program on Master Branch */
-        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x1 :");
+        x1 = sc.nextInt();
+        System.out.println("Enter the value of y1 :");
+        y1 = sc.nextInt();
+        System.out.println("Enter the value of x2 :");
+        x2 = sc.nextInt();
+        System.out.println("Enter the value of y2 :");
+        y2 = sc.nextInt();
+        double lengthofLine = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+        System.out.println("The length of a line using cartesian system = " +lengthofLine);
+
     }
 }
