455301851ddf1cb94445bdd377f13ee525262a6d
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-07 20:21:53 +0530

    ADDED_UC2Equalitycheck

--- src/LineComparisonDAY8.java
+++ src/LineComparisonDAY8.java
@@ -19,8 +19,30 @@ public class LineComparisonDAY8 {
         x2 = sc.nextInt();
         System.out.println("Enter the value of y2 :");
         y2 = sc.nextInt();
-        double lengthofLine = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
-        System.out.println("The length of a line using cartesian system = " +lengthofLine);
+        double lengthofLine1 = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+        System.out.println("The length of a line using cartesian system = " +lengthofLine1);
+
+        double x3,x4,y3,y4;
+        double LengthofLine2;
+        System.out.println("Enter the value of x3 :");
+        x3 = sc.nextInt();
+        System.out.println("Enter the value of y3 :");
+        y3 = sc.nextInt();
+        System.out.println("Enter the value of x4 :");
+        x4 = sc.nextInt();
+        System.out.println("Enter the value of y4 :");
+        y4 = sc.nextInt();
+        LengthofLine2 = Math.sqrt(Math.pow(x4-x3,2)+ Math.pow(y4-y3,2));
+        System.out.println("The length of a line =" +LengthofLine2);
+
+        String LineA=String.valueOf(lengthofLine1);
+        String LineB=String.valueOf(LengthofLine2);
+
+        if(LineA.equals(LineB)){
+            System.out.println("Length of both lines are equal");
+        }else{
+            System.out.println("lengths are not equal");
+        }
 
     }
 }
